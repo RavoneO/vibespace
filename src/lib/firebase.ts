e9637c36ef7ce10bde5e38a7397e41d24114e56f
Rakesh Mishra <rm6109903@gmail.com>
2025-09-06 19:51:15 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  projectId: "vibespace-h7vsa",
-  appId: "1:473324197014:web:880dfa26795f520da15eb7",
-  storageBucket: "vibespace-h7vsa.appspot.com",
-  apiKey: "AIzaSyB7n06b7mDNbBwhMkrVLIcYRuyFgRyH5jk",
-  authDomain: "vibespace-h7vsa.firebaseapp.com",
-  messagingSenderId: "473324197014"
+  "projectId": "vibespace-h7vsa",
+  "appId": "1:473324197014:web:880dfa26795f520da15eb7",
+  "storageBucket": "vibespace-h7vsa.appspot.com",
+  "apiKey": "AIzaSyB7n06b7mDNbBwhMkrVLIcYRuyFgRyH5jk",
+  "authDomain": "vibespace-h7vsa.firebaseapp.com",
+  "messagingSenderId": "473324197014"
 };
 
 // Initialize Firebase
