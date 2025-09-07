31276d98e911d5880894ac8763496a03dda89966
Rajbahadur rawat <rajbahadurrawat8889@gmail.com>
2025-09-07 15:14:06 +0000

    Convert webapp into app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="theme-color" content="#0F172A" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
