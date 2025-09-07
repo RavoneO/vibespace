9c06b004638cb5c563b280747508cd8cd94b0e28
Rajbahadur rawat <rajbahadurrawat8889@gmail.com>
2025-09-07 14:48:33 +0000

    Is this ready to publish on app store

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,21 @@
 
+"use client";
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  "projectId": "vibespace-h7vsa",
-  "appId": "1:473324197014:web:880dfa2_Super_Secret_AppId",
-  "storageBucket": "vibespace-h7vsa.appspot.com",
-  "apiKey": "AIzaSyB7n06b7mDNbBwhMkrVLIcYRuyFgRyH5jk",
-  "authDomain": "vibespace-h7vsa.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "473324197014"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
