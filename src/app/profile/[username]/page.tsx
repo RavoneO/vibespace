9d514ac0437d002158ceafff38ddbee6b378058e
Rakesh Mishra <rm6109903@gmail.com>
2025-09-07 06:46:30 +0000

    Same issue

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -1,10 +1,16 @@
 
+"use client";
 import { ProfileClientPage } from "./profile-client-page";
+import { useParams } from "next/navigation";
 
-export default function UserProfilePage({
-  params,
-}: {
-  params: { username: string };
-}) {
-  return <ProfileClientPage username={params.username} />;
+export default function UserProfilePage() {
+  const params = useParams();
+  const username = Array.isArray(params.username) ? params.username[0] : params.username;
+
+  if (!username) {
+    // You can render a loading state or null while waiting for params
+    return null;
+  }
+  
+  return <ProfileClientPage username={username} />;
 }
