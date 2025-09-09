4b1a82f690b3ff4cb68e7939f6a2c8f8767997b0
Rakesh Mishra <rm6109903@gmail.com>
2025-09-09 05:40:37 +0000

    Yes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
-        <meta name="theme-color" content="#0F172A" />
+        <meta name="theme-color" content="#020817" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
