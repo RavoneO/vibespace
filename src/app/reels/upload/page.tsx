347686be6b1101198ac5dd77f1a21551e5b13aaa
Rajbahadur rawat <rajbahadurrawat8889@gmail.com>
2025-09-10 20:48:20 +0000

    Review the changes from github, don't run command

--- src/app/reels/upload/page.tsx
+++ src/app/reels/upload/page.tsx
@@ -1,4 +1,4 @@
-import { UploadReelForm } from "./upload-reel-form";
+import { UploadReelForm } from "@/components/upload-reel-form";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { Icons } from "@/components/icons";
